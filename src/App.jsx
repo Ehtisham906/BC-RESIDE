a791ad42428dffe71eb93121b8c6aa4e3e10a274
Ehtisham <ehtishamzahid039@gmail.com>
2024-04-07 18:40:21 +0500

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import Guide from "./pages/Guide"
+import Mortage from "./pages/Mortage"
+import EditorLoft from "./pages/EditorLoft"
+import Contact from "./pages/Contact"
 
 export default function App() {
-  return (
-    <div className='text-pink-600 '>
-      App
-    </div>
-  )
+  return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/guide" element={<Guide />} />
+        <Route path="/mortage" element={<Mortage />} />
+        <Route path="/editorloft" element={<EditorLoft />} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
+  
 }
